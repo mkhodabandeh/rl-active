325392ba182117f62f14977e3ea40bc45ac91b55
sri3705 <smuralid@cs-vml-35.dyn.cs.sfu.ca>
2017-03-29 16:43:20 -0700

    added classifier.py

--- classifiers/Classifier.py
+++ classifiers/Classifier.py
@@ -1,15 +1,13 @@
-from lenet import LeNet
-
 class Classifier(object):
-    CLASSIFIERS = [LeNet]
+    CLASSIFIERS = ['LeNet']
 
     @classmethod
     def get_classifier(self, classifier_name, config_path):
         for cls in self.CLASSIFIERS:
             if cls.name == classifier_name:
                 return cls(config_path)
-        raise Exception('Classifier, {0}, is not existed in: {1}'.format(classifier_name ,str(self.get_classifiers_list())) 
-       
+        raise Exception('Classifier, {0}, is not existed in: {1}'.format(classifier_name ,str(self.get_classifiers_list())))
+    
     @classmethod
     def get_classifiers_list(self):
         return [cls.name for cls in self.CLASSIFIERS]
