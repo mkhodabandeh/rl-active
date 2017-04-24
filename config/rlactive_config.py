90de5fe2bd877045410badee571e45b16b1ce3de
sri3705 <smuralid@cs-vml-33.dyn.cs.sfu.ca>
2017-04-24 16:23:59 -0700

    add num data to config file

--- config/rlactive_config.py
+++ config/rlactive_config.py
@@ -1,17 +1,42 @@
-import rlactive_pb2
 from argparse import ArgumentParser
+#import aspy.yaml as yaml
+from aspy.yaml import ordered_dump
 
-PromptSolverConfig(args):
-    pass
+def PromptSolverConfig(args):
+    return {}
 
-PromptRLConfig(args)
-    pass
+def PromptRLConfig(args):
+    return {}
 
-try:
-    f = open(sys.argv[1], "rb")
-
+def PromptClassifierConfig(args):
+    relevant_args = ['NUM_DATA']
+    classifier_argdict = {}
+    for key in relevant_args: classifier_argdict.update({key:getattr(args, key)})
+    return classifier_argdict
 
 
 if __name__ == '__main__':
-    ArgParse = ArgumentParser()
-    ArgParse.add_argument('')
+    parser = ArgumentParser()
+    parser.add_argument('--NUM_DATA', type=int, default=1000,help='number of training images')
+    parser.add_argument('--OUTPUT_YAML', type=str, default='config.yml', help='yaml config filename')
+    args = parser.parse_args()
+    argdict = {}
+    solver_argdict = PromptSolverConfig(args)
+    rl_argdict = PromptRLConfig(args)
+    classifier_argdict = PromptClassifierConfig(args)
+    argdict.update(solver_argdict)
+    argdict.update(rl_argdict)
+    argdict.update(classifier_argdict)
+    yaml_obj = open(args.OUTPUT_YAML, 'w')
+    #ordered_dump(argdict, stream=yaml_obj)
+    yaml_obj.write('#Solver Args\n')
+    if solver_argdict:
+        rdered_dump(solver_argdict, stream=yaml_obj, default_flow_style=False)
+    yaml_obj.write('#RL Args\n')
+    if rl_argdict:
+        ordered_dump(rl_argdict, stream=yaml_obj, default_flow_style=False)
+    yaml_obj.write('#Classifier Args\n')
+    if classifier_argdict:
+        classifier_argdict = {'classifiers': classifier_argdict}
+        ordered_dump(classifier_argdict, stream=yaml_obj, default_flow_style=False)
+    yaml_obj.close()
